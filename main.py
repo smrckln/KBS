b4567cee03816d877e95edffe7f1f2a67cd6655f
sricklin1 <sam.ricklin@gmail.com>
2014-02-20 11:35:58 -0500

    Added in click events

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+import kivy.base
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -11,13 +12,16 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
 class MainScreen(GridLayout):
+    gov_names = dict()
+    att_gen_names = dict()
+    sup_court_names = dict()
+    votes = 0
+    
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.cols = 2
 
-        gov_names = dict()
-        att_gen_names = dict()
-        sup_court_names = dict()
+        
         
         gov_file = open("gov_names.txt", 'r')
         att_gen_file = open("att_gen_names.txt", 'r')
@@ -27,10 +31,11 @@ class MainScreen(GridLayout):
         v_layout.add_widget(Label(text = 'Governor/Lt. Governor'))
 
         for line in gov_file:
-            gov_names[line] = 0
+            line = line.replace('\n', '')
+            self.gov_names[line] = 0
             
             btn = ToggleButton(text = '%s' % (line), group='gov')
-            btn.id = "btn"
+            btn.id = "gov_btn"
                 
             v_layout.add_widget(btn)
 
@@ -38,10 +43,11 @@ class MainScreen(GridLayout):
         v_layout2.add_widget(Label(text= 'Attorney General'))
 
         for line in att_gen_file:
-            att_gen_names[line] = 0
+            line = line.replace('\n', '')
+            self.att_gen_names[line] = 0
             
             btn = ToggleButton(text = '%s' % (line), group='att_gen')
-            btn.id = "btn"
+            btn.id = "att_gen_btn"
             
                 
             v_layout2.add_widget(btn)
@@ -50,10 +56,11 @@ class MainScreen(GridLayout):
         v_layout3.add_widget(Label(text= 'Supreme Court Justice\n       (choose 3)'))
 
         for line in sup_court_file:
-            sup_court_names[line] = 0
+            line = line.replace('\n', '')
+            self.sup_court_names[line] = 0
             
             btn = ToggleButton(text = '%s' % (line))
-            btn.id = "btn"
+            btn.id = "sup_court_btn"
                 
             v_layout3.add_widget(btn)
 
@@ -65,6 +72,7 @@ class MainScreen(GridLayout):
         h_layout.add_widget(vote_btn)
         
         clear_btn = Button(text="Clear")
+        clear_btn.bind(on_press=self.clear_clicked)
         h_layout.add_widget(clear_btn)
         
         
@@ -73,7 +81,52 @@ class MainScreen(GridLayout):
         self.add_widget(v_layout3)
         self.add_widget(h_layout)
 
-    
+        
+
+    def vote_clicked(self, *args):
+        layout = self.children
+        for i in range(len(layout)):
+            temp = layout[i].children
+            for i in range(len(temp)):
+                if temp[i].id == 'gov_btn':
+                    if temp[i].state == 'down':
+                        self.gov_names[temp[i].text] += 1
+                if temp[i].id == 'att_gen_btn':
+                    if temp[i].state == 'down':
+                        self.att_gen_names[temp[i].text] += 1
+                if temp[i].id == 'sup_court_btn':
+                    if temp[i].state == 'down':
+                        self.sup_court_names[temp[i].text] += 1
+                        
+        print self.gov_names.items()
+        print self.att_gen_names.items()
+        print self.sup_court_names.items()
+        
+        self.votes += 1
+
+        
+
+    def clear_clicked(self, *args):
+        layout = self.children
+        for i in range(len(layout)):
+            temp = layout[i].children
+            for i in range(len(temp)):
+                if temp[i].id == 'gov_btn' or temp[i].id == 'att_gen_btn' or temp[i].id == 'sup_court_btn':
+                    if temp[i].state == 'down':
+                        temp[i].state = 'normal'
+
+    def exit_event(self, *args):
+        f = open('results.txt', 'w')
+        for item in self.gov_names.items():
+            f.write(item + '\n')
+        for item in self.att_gen_names.items():
+            f.write(item + '\n')
+        for item in self.sup_court_names.items():
+            f.write(item + '\n')
+                
+            
+        
+            
 
     
         
